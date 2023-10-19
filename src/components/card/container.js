ca80409110bb3353c2dc62d456a4139b2c20e1e3
dmytro1337mew <d.khabyuk@nltu.lviv.ua>
2023-10-20 02:21:41 +0300

    Fix CardContainer

--- src/components/card/container.js
+++ src/components/card/container.js
@@ -6,16 +6,16 @@ function CardContainer() {
   return (
     <div className="wrapper">
       <div className="containercard">
-        <input type="radio" name="slide" id="c1" />
+        <input type="radio" name="slide" id="c1" checked/>
         <Card id={1} title="1" />
 
-        <input type="radio" name="slide" id="c2" />
+        <input type="radio" name="slide" id="c2" checked/>
         <Card id={2} title="2"  />
 
-        <input type="radio" name="slide" id="c3" />
+        <input type="radio" name="slide" id="c3" checked/>
         <Card id={3} title="3"  />
 
-        <input type="radio" name="slide" id="c4" />
+        <input type="radio" name="slide" id="c4" checked/>
         <Card id={4} title="4"  />
       </div>
     </div>
