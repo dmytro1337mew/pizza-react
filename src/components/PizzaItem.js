0dc281686885d4f5716e8185d9ca5eda3d02b47d
dmytro1337mew <d.khabyuk@nltu.lviv.ua>
2023-10-24 19:39:40 +0300

    Адаптація під різні розширення

--- src/components/PizzaItem.js
+++ src/components/PizzaItem.js
@@ -21,7 +21,7 @@ function PizzaItem({ pizza }) {
           <h4 className="PizzaItemText">{pizza.name}</h4>
           <h4 className="PizzaItemDescription">{pizza.description.length > 100 ? pizza.description.substr(0, 30) + "..." : pizza.description}</h4>
           <div className='under'>
-          <h4 className='pizzaprice'>{pizza.price}</h4>
+          <h4 className='pizzaprice'>{pizza.price}₴</h4>
             {cart.some((c) => c.id === pizza.id) ? (
               <button onClick={() => removeItemFromCart(pizza)} className='PizzaItemButton1'>Видалити з замовлення</button>
             ) : (
