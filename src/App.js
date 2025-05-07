8542e65cdf87639f6ead837ded2b70216c766e73
Dmytro <105820092+dmytro1337mew@users.noreply.github.com>
2025-05-07 17:07:07 +0300

    Update App.js
    /main redirect from /

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
 import './App.css';
-import Header from './components/Header';
-import Body from './components/Body';
 import { Provider } from 'react-redux';
 import store from './store';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoadingScreen from './screens/LoadingScreen';
 import Home from './screens/Home';
 import Cart from './screens/Cart';
 import About from './screens/About';
 import EmailPage from './screens/EmailPage';
-import LoadingScreen from './screens/LoadingScreen';
-import { useHistory } from 'react-router-dom';
-import baseURL from "./components/login/api/axios";
-import axios from 'axios';
-import { subscribeTokenRefresh, onRefreshed } from './components/login/interseptor';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
 import AdminPage from './screens/AdminPage';
 import Order from './screens/Order';
 
 function App() {
-  
   return (
     <div className="app">
       <BrowserRouter>
-      <Provider store={store}>
-<Routes>
-<Route path="/pizza-react" element={<LoadingScreen/>} />
-<Route path="/adminpage" element={<AdminPage/>}/>
-  <Route path="/main" element={<Home/>}/>
-  <Route path="/cart" element ={<Cart/>}/>
-  <Route path="/email" element ={<EmailPage/>}/>
-  <Route path="/about" element ={<About/>}/>
-  <Route path="/orders" element ={<Order/>}/>
-</Routes>
-      </Provider>
+        <Provider store={store}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/main" />} />
+            <Route path="/pizza-react" element={<LoadingScreen />} />
+            <Route path="/adminpage" element={<AdminPage />} />
+            <Route path="/main" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/email" element={<EmailPage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/orders" element={<Order />} />
+          </Routes>
+        </Provider>
       </BrowserRouter>
     </div>
   );
